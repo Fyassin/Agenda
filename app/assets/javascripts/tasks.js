ec53668b95263da023d89cad157ad347a8868ea1
Faneeza Yassin <faneeza.yassin@gmail.com>
2019-09-19 16:54:37 +0000

    minor changes

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -65,6 +65,7 @@ $(function() {
       var ulAgendas = $('.agenda-list');
       ulAgendas.append(htmlString);
       $('.toggle').click(toggleTask);
+      $('.new-todo').val('');
     });
   });
 
